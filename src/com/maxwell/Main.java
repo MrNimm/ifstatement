d26df6f1b0e913ae0cd43396c106c2ce955a4e08
Maxwell <maxwell.guru.1@gmail.com>
2021-03-16 17:59:52 -0600

    Initial commit

---
+++ src/com/maxwell/Main.java
@@ -0,0 +1,15 @@
+package com.maxwell;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    //Enter input
+
+    //Check
+
+    //Check even
+
+    }
+}
