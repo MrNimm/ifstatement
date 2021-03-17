c57c187ae6f8265697f2544beba2621daecfe98a
Maxwell <maxwell.guru.1@gmail.com>
2021-03-16 18:02:20 -0600

    Scanner input and user input

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -1,12 +1,13 @@
 package com.maxwell;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
     //Enter input
-
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
     //Check
 
     //Check even
